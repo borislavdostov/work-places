e562991ba6aca91139045e87b4e203d745390982
Borislav Dostov <bdostov@gmail.com>
2021-09-16 17:29:58 +0300

    Create getWorkPlaces method

--- src/app/work-place/work-place.service.ts
+++ src/app/work-place/work-place.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IUserWorkPlace } from '../shared/interfaces/user-work-place';
+import { environment } from 'src/environments/environment';
+const apiUrl = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkPlaceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWorkPlaces(): Observable<IUserWorkPlace[]>{
+    return this.http.get<IUserWorkPlace[]>(`${apiUrl}/userworkplaces`);
+  }
 }
