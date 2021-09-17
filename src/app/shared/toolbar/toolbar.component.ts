fc846d3ef1b5852a9e87611e6ce23374bb4569fb
Borislav Dostov <bdostov@gmail.com>
2021-09-17 15:35:19 +0300

    Refactoring

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -8,7 +8,6 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class ToolbarComponent{
 
   @Input() title = '';
-  @Input() buttonTitle = '';
   @Output() newClicked = new EventEmitter();
 
   constructor() { }
