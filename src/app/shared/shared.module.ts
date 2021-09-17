cb02825cef5ee69c46864a5e2547dd52b2058eda
Borislav Dostov <bdostov@gmail.com>
2021-09-17 10:51:54 +0300

    Bug fixes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,13 +10,11 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoaderComponent } from './loader/loader.component';
 import { RouterModule } from '@angular/router';
-import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
 
 @NgModule({
   declarations: [
     NotFoundComponent,
-    LoaderComponent,
-    DeleteDialogComponent
+    LoaderComponent
   ],
   imports: [
     CommonModule,
