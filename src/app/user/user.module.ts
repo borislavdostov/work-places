2aa0942d3c77351eafdbd6000c53b73794b6384a
Borislav Dostov <bdostov@gmail.com>
2021-09-17 15:52:17 +0300

    Bug fixes

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
 
 import { SharedModule } from '../shared/shared.module';
-import { AddEditUserComponent } from './add-edit-user/add-edit-user.component';
 import { AddEditUserDialogComponent } from './add-edit-user-dialog/add-edit-user-dialog.component';
 
 @NgModule({
   declarations: [
     UserListComponent,
-    AddEditUserComponent,
     AddEditUserDialogComponent
   ],
   imports: [
