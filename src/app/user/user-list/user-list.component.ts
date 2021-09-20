a6b1fa10d0e8af8ed97fe23edc7023671a0d979c
Borislav Dostov <bdostov@gmail.com>
2021-09-20 14:49:38 +0300

    Additional validation added

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -86,7 +86,7 @@ export class UserListComponent implements OnInit {
         this.getUsers();
       },
       error: (error) => {
-        alert(error)
+        alert("Implement backend errors:" + error)
       }
     });
   }
