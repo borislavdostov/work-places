456bb1fcdd85f0ff0d71ab31c59e389578b81403
Borislav Dostov <bdostov@gmail.com>
2021-09-20 15:43:48 +0300

    Add user work places options method in service

---
+++ src/app/shared/interfaces/user-work-place-options.ts
@@ -0,0 +1,4 @@
+export interface IUserWorkPlaceOptions {
+    users: [],
+    workPlaces: []
+}
--- src/app/work-place/work-place.service.ts
+++ src/app/work-place/work-place.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IUserWorkPlace } from '../shared/interfaces/user-work-place';
 import { environment } from 'src/environments/environment';
+import { IUserWorkPlaceOptions } from '../shared/interfaces/user-work-place-options';
 const apiUrl = environment.apiUrl;
 
 @Injectable({
@@ -14,6 +15,10 @@ export class WorkPlaceService {
 
   getWorkPlaces(): Observable<IUserWorkPlace[]> {
     return this.http.get<IUserWorkPlace[]>(`${apiUrl}/userworkplaces`);
+  }
+
+  getWorkPlaceOptions(): Observable<IUserWorkPlaceOptions[]> {
+    return this.http.get<IUserWorkPlaceOptions[]>(`${apiUrl}/userworkplaces/options`);
   }
 
   deleteWorkPlace(id: number) {
