caac984940a400388894ec7635230d9a5c2cfc11
Borislav Dostov <bdostov@gmail.com>
2021-09-21 10:13:23 +0300

    Update user-list.component.ts

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -11,7 +11,7 @@ import { UserService } from '../user.service';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent implements OnInit {
 
