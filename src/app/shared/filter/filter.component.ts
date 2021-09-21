c6afc39efe6377d8c8399ef8a7327ff48d68953d
Borislav Dostov <bdostov@gmail.com>
2021-09-21 15:25:15 +0300

    Bug fixes

--- src/app/shared/filter/filter.component.ts
+++ src/app/shared/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
@@ -13,8 +13,12 @@ export class FilterComponent {
 
   constructor() { }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  applyFilter(input: HTMLInputElement) {
+    this.dataSource.filter = input.value.trim().toLowerCase();
+  }
+
+  onClearClick(input: HTMLInputElement){
+    input.value = '';
+    this.applyFilter(input);
   }
 }
