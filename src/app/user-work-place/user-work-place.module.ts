71669a128a0376bf74e80f615b6aa8622b92697a
Borislav Dostov <bdostov@gmail.com>
2021-09-21 15:52:26 +0300

    Update user-work-place.module.ts

--- src/app/user-work-place/user-work-place.module.ts
+++ src/app/user-work-place/user-work-place.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserWorkPlaceListComponent } from './user-work-place-list/user-work-place-list.component';
 import { SharedModule } from '../shared/shared.module';
+
+import { UserWorkPlaceListComponent } from './user-work-place-list/user-work-place-list.component';
 import { AddEditUserWorkPlaceDialogComponent } from './add-edit-user-work-place-dialog/add-edit-user-work-place-dialog.component';
 
 @NgModule({
