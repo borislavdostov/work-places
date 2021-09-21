337b5ad02f317bbfe3646588026f96df234bb50e
Borislav Dostov <bdostov@gmail.com>
2021-09-21 15:38:48 +0300

    Update filter.component.ts

--- src/app/shared/filter/filter.component.ts
+++ src/app/shared/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
