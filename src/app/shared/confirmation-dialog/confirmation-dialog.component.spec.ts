c2cd27ce1724f98c5f03018b50ac1745632a5a63
Borislav Dostov <bdostov@gmail.com>
2021-09-28 14:59:18 +0300

    Update confirmation-dialog.component.spec.ts

--- src/app/shared/confirmation-dialog/confirmation-dialog.component.spec.ts
+++ src/app/shared/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
@@ -13,7 +14,8 @@ describe('ConfirmationDialogComponent', () => {
       providers: [
         { provide: MAT_DIALOG_DATA, useValue: {} },
         { provide: MatDialogRef, useValue: {} }
-      ]
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
       .compileComponents();
   });
