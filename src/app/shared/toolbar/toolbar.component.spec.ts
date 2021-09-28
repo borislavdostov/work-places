5dd377be97137d6a8cf6dbe0a89b0eabbf8750cf
Borislav Dostov <bdostov@gmail.com>
2021-09-28 14:59:22 +0300

    Update toolbar.component.spec.ts

--- src/app/shared/toolbar/toolbar.component.spec.ts
+++ src/app/shared/toolbar/toolbar.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ToolbarComponent } from './toolbar.component';
@@ -8,7 +9,8 @@ describe('ToolbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ToolbarComponent ]
+      declarations: [ ToolbarComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   });
