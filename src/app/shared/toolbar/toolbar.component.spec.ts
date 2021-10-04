2a99d29b04b6b36aab73edd3b740072685c27574
Borislav Dostov <bdostov@gmail.com>
2021-10-04 10:12:27 +0300

    Add 2 tests for toolbar

--- src/app/shared/toolbar/toolbar.component.spec.ts
+++ src/app/shared/toolbar/toolbar.component.spec.ts
@@ -9,10 +9,10 @@ describe('ToolbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ToolbarComponent ],
+      declarations: [ToolbarComponent],
       schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -23,5 +23,23 @@ describe('ToolbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set title correctly', () => {
+    component.title = 'Toolbar Title';
+    fixture.detectChanges();
+
+    let title = fixture.debugElement.nativeElement.querySelector('h1').textContent;
+
+    expect(title).toEqual('Toolbar Title');
+  });
+
+  it('should call buttonHandler method when New button is clicked', () => {
+    let button = fixture.debugElement.nativeElement.querySelector('button');
+    spyOn(component, 'buttonHandler');
+
+    button.click();
+
+    expect(component.buttonHandler).toHaveBeenCalled();
   });
 });
