cddaf6b5cfb8b1f359b3b8feb59ec9f26fba21fc
Borislav Dostov <bdostov@gmail.com>
2021-10-05 15:56:33 +0300

    Update user-workplace-list.component.ts

--- src/app/user-workplace/user-workplace-list/user-workplace-list.component.ts
+++ src/app/user-workplace/user-workplace-list/user-workplace-list.component.ts
@@ -78,7 +78,7 @@ export class UserWorkplaceListComponent implements OnInit, AfterViewInit {
       userWorkplace => {
         let dialogRef = this.addEditWorkplaceDialog.open(AddEditUserWorkplaceDialogComponent, {
           data: {
-            title: `Edit Workplace` + userWorkplace,
+            title: `Edit Workplace`,
             userWorkplaceId: userWorkplaceId,
             userWorkplace: userWorkplace
           }
