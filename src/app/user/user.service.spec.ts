d9ac8e2bca228e5f6bbb21c07707c2cf030aa2d7
Borislav Dostov <bdostov@gmail.com>
2021-10-05 13:30:36 +0300

    Update user.service.spec.ts

--- src/app/user/user.service.spec.ts
+++ src/app/user/user.service.spec.ts
@@ -1,18 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 import { UserService } from './user.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
+
+const apiUrl = environment.apiUrl;
 
 describe('UserService', () => {
   let service: UserService;
+  let mockHttp: HttpTestingController;
+
+  let dummyUsers = [
+    { id: 1, name: "John", email: "example@email.com", age: 25 },
+    { id: 2, name: "Peter", email: "example@email.com", age: 34 },
+    { id: 3, name: "Simon", email: "example@email.com", age: 19 },
+  ];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule]
     });
     service = TestBed.inject(UserService);
+    mockHttp = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should make get request', () => {
+    service.getUsers().subscribe(() => {
+    });
+    const req = mockHttp.expectOne(`${apiUrl}/users`);
+
+    req.flush(dummyUsers);
+
+    expect(req.request.method).toBe("GET");
+  });
+
 });
